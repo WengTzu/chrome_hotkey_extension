b91a18f90fdf354f1b229941e03a6fb6724b8d1c
TJ <x54387913@gmail.com>
2024-07-22 00:23:41 +0800

    event functional update

--- event.js
+++ event.js
@@ -1,41 +1,77 @@
 var toggleBg = false;
-
 let port = null
-
+let selectedTextGlobal = "";
 
 // chrome.commands.onCommand.addListener(newTab);
+
 
 chrome.commands.onCommand.addListener(function (command, tab) {
 
     console.log(tab.id);
     console.log('Command:', command);
 
+    function getSelectedTextFromPage() {
+        // Retrieve the current text selection from the window
+        const selection = window.getSelection().toString();
+        // Process the selected text (this step is optional and can be customized)
+        // For demonstration, let's just return the trimmed version of the selected text
+        // return selectedText.trim();
+
+        if (selection) {
+            return selection.trim(); // Return the selection if it's not empty
+        } else {
+            // // If the main window has no selection, check iframes
+            // const iframes = document.getElementsByTagName('iframe');
+            // for (let i = 0; i < iframes.length; i++) {
+            //     try {
+            //         // Attempt to access iframe content
+            //         const iframeDoc = iframes[i].contentDocument || iframes[i].contentWindow.document;
+            //         const iframeSelection = iframeDoc.getSelection().toString();
+            //         if (iframeSelection) {
+            //             return iframeSelection; // Return the selection from the iframe
+            //         }
+            //     } catch (e) {
+            //         console.error('Error accessing iframe content:', e);
+            //         // Iframe content might not be accessible due to same-origin policy
+            //     }
+            // }
+        }
+    }
+
+    chrome.scripting.executeScript({
+        target: { tabId: tab.id },
+        function: getSelectedTextFromPage
+    }, (results) => {
+        // results[0].result will contain the value returned by getSelectedTextFromPage
+        selectedTextGlobal = results[0].result;
+        console.log('Selected text:', results[0].result);
+    });
+
     //const tabId = getTabId()
     if (command == "copy-magnet-name") {
-        chrome.scripting.executeScript({
-            target: { tabId: tab.id },
-            files: ['copy.js'],
-        });
+        // chrome.scripting.executeScript({
+        //     target: { tabId: tab.id },
+        //     files: ['copy.js'],
+        // });
 
-        setTimeout(function () {
-            port = chrome.runtime.connectNative("com.magnet_autocopy.myauto")
-            port.onMessage.addListener(function (msg) {
-                console.log("Received" + msg);
-            });
-            port.onDisconnect.addListener(function () {
-                console.log("Disconnected");
-                if (chrome.runtime.lastError) {
-                    console.log(chrome.runtime.lastError);
-                }
-            });
-        }, 2000);
-
+        // setTimeout(function () {
+        //     port = chrome.runtime.connectNative("com.magnet_autocopy.myauto")
+        //     port.onMessage.addListener(function (msg) {
+        //         console.log("Received" + msg);
+        //     });
+        //     port.onDisconnect.addListener(function () {
+        //         console.log("Disconnected");
+        //         if (chrome.runtime.lastError) {
+        //             console.log(chrome.runtime.lastError);
+        //         }
+        //     });
+        // }, 2000);
     }
 
     if (command == "search_on_jav") {
-        async function getElementsWithoutOpen() {
+        async function getElementsWithoutOpen(sid) {
             console.log('search on jav')
-            var sid = window.getSelection().toString();
+            // var sid = selectedTextGlobal
             console.log("get selected", sid);
 
             // let sids = selected.split(" ");
@@ -89,14 +125,15 @@ chrome.commands.onCommand.addListener(function (command, tab) {
         chrome.scripting.executeScript({
             target: { tabId: tab.id },
             function: getElementsWithoutOpen,
+            args: [selectedTextGlobal],
         });
     }
     
 
     if (command == "search_on_yahoo") {
         console.log('search on yahoo')
-        function getselected() {
-            var selected = window.getSelection().toString()
+        function yahoo(selected) {
+            // var selected = selectedTextGlobal
             console.log("get selected", selected)
             url = 'https://tw.dictionary.search.yahoo.com/search?p=' + selected
             window.open(url, '_blank').focus();
@@ -104,14 +141,15 @@ chrome.commands.onCommand.addListener(function (command, tab) {
         }
         chrome.scripting.executeScript({
             target: { tabId: tab.id },
-            func: getselected
+            func: yahoo,
+            args: [selectedTextGlobal],
         });
     }
 
     if (command == "search_on_skell") {
         console.log('search on skell')
-        function getselected() {
-            var selected = window.getSelection().toString()
+        function getselected(selected) {
+            // var selected = selectedTextGlobal
             console.log("get selected", selected)
             url = 'https://skell.sketchengine.eu/#result?f=wordsketch&lang=en&query=' + selected
             window.open(url, '_blank').focus();
@@ -119,16 +157,15 @@ chrome.commands.onCommand.addListener(function (command, tab) {
         }
         chrome.scripting.executeScript({
             target: { tabId: tab.id },
-            func: getselected
+            func: getselected,
+            args: [selectedTextGlobal],
         });
     }
 
     if (command == "search_on_cambridge") {
         console.log('search on cambridge')
-        function getselected() {
-            console.log("Windows selectedText", window.getSelection());
-
-            var selected = window.getSelection().toString()
+        function getselected(selected) {
+            // var selected = selectedTextGlobal  
             console.log("get selected", selected)
             url = 'https://dictionary.cambridge.org/zht/%E8%A9%9E%E5%85%B8/%E8%8B%B1%E8%AA%9E-%E6%BC%A2%E8%AA%9E-%E7%B9%81%E9%AB%94/' + selected
             window.open(url, '_blank').focus();
@@ -136,14 +173,15 @@ chrome.commands.onCommand.addListener(function (command, tab) {
         }
         chrome.scripting.executeScript({
             target: { tabId: tab.id },
-            func: getselected
+            func: getselected,
+            args: [selectedTextGlobal],
         });
     }
 
     if (command == "search_on_longman") {
         console.log('search on longman')
-        function getselected() {
-            var selected = window.getSelection().toString()
+        function getselected(selected) {
+            // var selected = selectedTextGlobal
             console.log("get selected", selected)
             url = 'https://www.ldoceonline.com/dictionary/' + selected
             window.open(url, '_blank').focus();
@@ -151,14 +189,15 @@ chrome.commands.onCommand.addListener(function (command, tab) {
         }
         chrome.scripting.executeScript({
             target: { tabId: tab.id },
-            func: getselected
+            func: getselected,
+            args: [selectedTextGlobal],
         });
     }
 
     if (command == "search_on_wordhippo") {
         console.log('search on wordhippo')
-        function getselected() {
-            var selected = window.getSelection().toString()
+        function getselected(selected) {
+            // var selected = selectedTextGlobal
             console.log("get selected", selected)
             url = 'https://www.wordhippo.com/what-is/another-word-for/' + selected + '.html'
             window.open(url, '_blank').focus();
@@ -166,14 +205,15 @@ chrome.commands.onCommand.addListener(function (command, tab) {
         }
         chrome.scripting.executeScript({
             target: { tabId: tab.id },
-            func: getselected
+            func: getselected,
+            args: [selectedTextGlobal],
         });
     }
 
     if (command == "search_on_google") {
         console.log('search on google')
-        function getselected() {
-            var selected = window.getSelection().toString()
+        function getselected(selected) {
+            // var selected = selectedTextGlobal
             console.log("get selected", selected)
             url = 'https://translate.google.com/?sl=auto&tl=zh-TW&text=' + selected + '&op=translate'
             window.open(url, '_blank').focus();
@@ -181,7 +221,8 @@ chrome.commands.onCommand.addListener(function (command, tab) {
         }
         chrome.scripting.executeScript({
             target: { tabId: tab.id },
-            func: getselected
+            func: getselected,
+            args: [selectedTextGlobal],
         });
     }
 
